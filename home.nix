6a1adf44fc1066fa91eabcd074946150805577dd
marcosrdac <marcosrdac@gmail.com>
2022-02-23 15:38:48 -0300

    trying to add polybar

--- home.nix
+++ home.nix
@@ -1,24 +1,116 @@
 { config, pkgs, ... }:
 
 {
-  home.username = "marcosrdac";
-  home.homeDirectory = "/home/marcosrdac";
-  home.stateVersion = "21.05";
-  home.packages = with pkgs; [ ];
-  #programs.home-manager.enable = true;
+  programs.home-manager.enable = true;
 
-  home.file."test".text = "a test";      # does not work
+  home.packages = with pkgs; [
+    lf
 
-  xsession.enable = true;
-  xsession.scriptPath = ".hm-xsession";  # does not work
+    dmenu
+    brightnessctl  # adjust lights
+  ];
 
-  xsession.windowManager.bspwm = {
+  #home.language.base = "us";
+
+  home.keyboard = {
+    layout = "us";
+    variant = "intl";
+    options = [ "caps:swapescape" ];
+  };
+
+  #home.shellAliases = { };  # not yet valid
+
+  xsession = {
     enable = true;
-    settings = { "split_ratio" = 0.6; };
-    monitors = { };
-    rules = { };
-    extraConfig = "";
-    startupPrograms = [ "sxhkd" ];
+    scriptPath = ".xsession-hm";
+
+    windowManager.bspwm = {
+      enable = true;
+      extraConfig = builtins.readFile ./config/bspwm/bspwmrc;
+    };
+  };
+  services.sxhkd = {
+    enable = true;
+    extraConfig = builtins.readFile ./config/sxhkd/sxhkdrc;
+  };
+
+  services.polybar = {
+    enable = true;
+    script = builtins.readFile ./config/polybar/config;
+  };
+
+  programs.alacritty.enable = true;
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    extraConfig = builtins.concatStringsSep "\n" [
+    #  (lib.strings.fileContents ./base.vim)
+    #  (lib.strings.fileContents ./plugins.vim)
+    #  (lib.strings.fileContents ./lsp.vim)
+    #  # this allows you to add lua config files
+    #  ''
+    #    lua << EOF
+    #    ${lib.strings.fileContents ./config.lua}
+    #    ${lib.strings.fileContents ./lsp.lua}
+    #    EOF
+    #  ''
+    ];
+    extraPackages = with pkgs; [
+      # used to compile tree-sitter grammar
+      #tree-sitter
+
+      # installs different langauge servers for neovim-lsp
+      # have a look on the link below to figure out the ones for your languages
+      # https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md
+      #nodePackages.typescript nodePackages.typescript-language-server
+      #gopls
+      #nodePackages.pyright
+      #rust-analyzer
+    ];
+    plugins = with pkgs.vimPlugins; [
+      # you can use plugins from the pkgs
+      vim-which-key
+      #vim-quick-scope
+
+      ## or you can use our function to directly fetch plugins from git
+      #(plugin "neovim/nvim-lspconfig")
+      #(plugin "hrsh7th/nvim-compe") # completion
+      #(plugin "Raimondi/delimitMate") # auto bracket
+
+      ## this installs the plugin from 'lua' branch
+      #(pluginGit "lua" "lukas-reineke/indent-blankline.nvim")
+
+      ## syntax highlighting
+      #(plugin "nvim-treesitter/nvim-treesitter")
+      #(plugin "p00f/nvim-ts-rainbow") # bracket highlighting
+    ];
+  };
+  
+  home.sessionVariables = {
+    TERMINAL = "alacritty";
+    BROWSER = "qutebrowser";
+
+
+  };
+
+  # home.file.".vimrc".text = " ...";
+  # xdg.configFile."sxhkd/sxhkdrc".source = ./config/sxhkd/sxhkdrc;
+  
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      # home-manager
+      hm = "home-manager";
+      hms = "home-manager switch --flake ${config.xdg.configHome}/nixpkgs#`hostname`";
+      ehm = "$EDITOR ${config.xdg.configHome}/nixpkgs/home.nix";
+
+      ll = "ls -l";
+    };
+
+    history = {
+      size = 10000;
+      path = "${config.xdg.dataHome}/zsh/history";
+    };
   };
 
   programs.git = {
@@ -31,28 +123,3 @@
   };
 
 }
-
-
-
-    #package = pkgs.bspwm;
-
-#  programs.zsh = {
-#    enable = true;
-#    shellAliases = {
-#      hm = "home-manager";
-#      ll = "ls -l";
-#    };
-#
-#    history = {
-#      size = 10000;
-#      path = "${config.xdg.dataHome}/zsh/history";
-#    };
-#  };
-#
-
-
-#  home.file = {
-#    #".vimrc".source = ./../../dotfiles/vim/vimrc;
-#    ".vimrc".text = " ";
-#  };
-
